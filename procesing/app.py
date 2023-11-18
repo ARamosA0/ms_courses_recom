693a3679ff914def9fdd5b0f66e97328bee527ba
ARamosA0 <aldoramosaro@gmail.com>
2023-11-18 13:00:06 -0500

    arq falta corregir kafka

---
+++ procesing/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, make_response, g
+from redis import Redis
+import os
+import socket
+import random
+import json
+import logging
+
+hostname = socket.gethostname()
+
+app = Flask(__name__)
+
+gunicorn_error_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers.extend(gunicorn_error_logger.handlers)
+app.logger.setLevel(logging.INFO)
+
+def get_redis():
+    if not hasattr(g, 'redis'):
+        #cambiar el puerto por 6380 para comectarce al otro redis
+        g.redis = Redis(host="redis", port=6379, db=0, socket_timeout=5)
+    return g.redis
+
+@app.route("/", methods=['POST','GET'])
+def hello():
+  return 
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
