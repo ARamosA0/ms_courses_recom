5f929782d4ed2c431bb4ced8cc4236dc7e6a0192
ARamosA0 <aldoramosaro@gmail.com>
2023-11-21 11:32:19 -0500

    api gaeway

--- cursos/app.py
+++ cursos/app.py
@@ -1,29 +1,48 @@
-from flask import Flask, render_template, request, make_response, g
-from redis import Redis
-import os
-import socket
-import random
-import json
-import logging
+# from flask import Flask, render_template, request, make_response, g
+# from redis import Redis
+# import os
+# import socket
+# import random
+# import json
+# import logging
+#
+# hostname = socket.gethostname()
+#
+# app = Flask(__name__)
+#
+# gunicorn_error_logger = logging.getLogger('gunicorn.error')
+# app.logger.handlers.extend(gunicorn_error_logger.handlers)
+# app.logger.setLevel(logging.INFO)
+#
+# def get_redis():
+#     if not hasattr(g, 'redis'):
+#         #cambiar el puerto por 6380 para comectarce al otro redis
+#         g.redis = Redis(host="redis", port=6379, db=0, socket_timeout=5)
+#     return g.redis
+#
+# @app.route("/", methods=['POST','GET'])
+# def index():
+#   return json.dumps({'name': 'alice',
+#                        'email': 'alice@outlook.com'})
+#     
+#
+#
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=81, debug=True, threaded=True)
 
-hostname = socket.gethostname()
+#!/usr/bin/env python
+# encoding: utf-8
+import json
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-gunicorn_error_logger = logging.getLogger('gunicorn.error')
-app.logger.handlers.extend(gunicorn_error_logger.handlers)
-app.logger.setLevel(logging.INFO)
-
-def get_redis():
-    if not hasattr(g, 'redis'):
-        #cambiar el puerto por 6380 para comectarce al otro redis
-        g.redis = Redis(host="redis", port=6379, db=0, socket_timeout=5)
-    return g.redis
-
-@app.route("/", methods=['POST','GET'])
-def hello():
-  return 
-
+@app.route('/')
+def index():
+    return jsonify({'message':{'name': 'alice',
+                    'email': 'alice@outlook.com'}})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=81, debug=True, threaded=True)
