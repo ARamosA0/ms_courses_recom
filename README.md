30edb7525bd8e075ade83f65ba17fe8df436eac9
ARamosA0 <aldoramosaro@gmail.com>
2023-11-24 11:54:40 -0500

    nuevo dotnet y guia de kong

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # **ARQUITECTURA DE MS CON KAFKA**
 
+---
+
 ## **Diagramas**
 
 - [Diagramas](https://miro.com/welcomeonboard/a3dXYmtpaVpZcVhzaWNWaDZlYUVEd1dvS2I0SFVlekhwbGkxdlJnUE9sVEgxWFczb0FDZW56WVdFR2VYajltdnwzNDU4NzY0NTM0Mjk4NTg0NjA0fDI=?share_link_id=519012505508)
@@ -8,11 +10,97 @@
 
 - [Front](https://www.figma.com/proto/Ot6bc82rw3QlpVmaINKZms/UI-Design-System-Recomendation?type=design&node-id=49-422&t=pK2g5gepaGibhshK-0&scaling=scale-down&page-id=0%3A1)
 
+---
+
 ## **Comandos**
 
 - docker compose up -d 
 
+---
 
 ## **Puertos**
 
 - [front localhost:3000](http://localhost:3000)
+
+---
+
+## **API GATEWAY**
+
+La API Gateway esta echa en KONG usando una base de datos PosrtgeSQL 
+
+#### **Comandos para conectar los servicios**
+
+Hacer estos comandos para crear servicios y routes.
+
+Hay dos formas para hacer esto, mediante postman y mediante terminal
+
+Para ambas es importante conocer la IP de tu computadora, **IMPORTANTE** reemplazar *IP* de las urls
+
+##### *POSTMAN*
+
+**Load Balancer**
+
+- {http://localhost:8001/upstreams}
+- {
+    {
+    "name":"loadbalancer_upstream"
+    }
+}
+- **IMPORTANTE** Cambiar el puerto con 5001, 5002 
+- {http://localhost:8001/upstreams/loadbalancer_upstream/targets}
+- {
+    {
+    "target":"*IP*:5000"
+    }
+}
+
+
+**SERVICIOS**
+- {http://localhost:8001/services/}
+- {
+    {
+    "name":"usuarios",
+    "url": "http://*IP*:5001",
+    "host":"loadbalancer_upstream"
+    }
+}
+- {http://localhost:8001/services/}
+- {
+    {
+    "name":"cursos",
+    "url": "http://*IP*:5002",
+    "host":"loadbalancer_upstream"
+    }
+}
+- {http://localhost:8001/services/}
+- {
+    {
+    "name":"procesar",
+    "url": "http://*IP*:5000",
+    "host":"loadbalancer_upstream"
+    }
+}
+- {http://localhost:8001/services/usuarios/routes}
+- {
+    {
+    "name":"usuarios-route",
+    "paths":["/usuarios"]
+    }   
+}
+- {http://localhost:8001/services/cursos/routes}
+- {
+    {
+    "name":"cursos-route",
+    "paths":["/cursos"]
+    }   
+}
+- {http://localhost:8001/services/procesar/routes}
+- {
+    {
+    "name":"procesar-route",
+    "paths":["/procesar"]
+    }   
+}
+
+
+
--- collect/Program.cs
+++ collect/Program.cs
@@ -1,159 +1,25 @@
-using System;
-using System.Data.Common;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading;
-using Newtonsoft.Json;
-using Npgsql;
-using StackExchange.Redis;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace Collect
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static int Main(string[] args)
-        {
-            try
-            {
-                var pgsql = OpenDbConnection("Server=db;Username=postgres;Password=postgres;");
-                var redisConn = OpenRedisConnection("redis");
-                var redis = redisConn.GetDatabase();
-
-                // Keep alive is not implemented in Npgsql yet. This workaround was recommended:
-                // https://github.com/npgsql/npgsql/issues/1214#issuecomment-235828359
-                var keepAliveCommand = pgsql.CreateCommand();
-                keepAliveCommand.CommandText = "SELECT 1";
-
-                var definition = new { vote = ""};
-                while (true)
-                {
-                    // Slow down to prevent CPU spike, only query each 100ms
-                    Thread.Sleep(100);
-
-                    // Reconnect redis if down
-                    if (redisConn == null || !redisConn.IsConnected) {
-                        Console.WriteLine("Reconnecting Redis");
-                        redisConn = OpenRedisConnection("redis");
-                        redis = redisConn.GetDatabase();
-                    }
-                    string json = redis.ListLeftPopAsync("distancia").Result;
-                   // Console.WriteLine(json);
-                    if (json != null)
-                    {
-                        Console.WriteLine(json);
-                        // Reconnect DB if down
-                        if (!pgsql.State.Equals(System.Data.ConnectionState.Open))
-                        {
-                            Console.WriteLine("Reconnecting DB");
-                            pgsql = OpenDbConnection("Server=db;Username=postgres;Password=postgres;");
-                        }
-                        else
-                        { // Normal +1 vote requested
-                          
-                            Console.WriteLine(json);
-                            UploadPostgres(pgsql, json);
-                        }
-                    }
-                    else
-                    {
-                        keepAliveCommand.ExecuteNonQuery();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine(ex.ToString());
-                return 1;
-            }
-        }
-
-        private static NpgsqlConnection OpenDbConnection(string connectionString)
-        {
-            NpgsqlConnection connection;
-
-            while (true)
-            {
-                try
-                {
-                    connection = new NpgsqlConnection(connectionString);
-                    connection.Open();
-                    break;
-                }
-                catch (SocketException)
-                {
-                    Console.Error.WriteLine("Waiting for db");
-                    Thread.Sleep(1000);
-                }
-                catch (DbException)
-                {
-                    Console.Error.WriteLine("Waiting for db");
-                    Thread.Sleep(1000);
-                }
-            }
-
-            Console.Error.WriteLine("Connected to db");
-
-            var command = connection.CreateCommand();
-            command.CommandText = @"CREATE TABLE IF NOT EXISTS votes (
-                                        vote VARCHAR(255) NOT NULL
-                                    )";
-            command.ExecuteNonQuery();
-
-            return connection;
-        }
-
-        private static ConnectionMultiplexer OpenRedisConnection(string hostname)
-        {
-            // Use IP address to workaround https://github.com/StackExchange/StackExchange.Redis/issues/410
-            var ipAddress = GetIp(hostname);
-            Console.WriteLine($"Found redis at {ipAddress}");
-
-            while (true)
-            {
-                try
-                {
-                    Console.Error.WriteLine("Connecting to redis");
-                    return ConnectionMultiplexer.Connect(ipAddress);
-                }
-                catch (RedisConnectionException)
-                {
-                    Console.Error.WriteLine("Waiting for redis");
-                    Thread.Sleep(1000);
-                }
-            }
-        }
-
-        private static string GetIp(string hostname)
-            => Dns.GetHostEntryAsync(hostname)
-                .Result
-                .AddressList
-                .First(a => a.AddressFamily == AddressFamily.InterNetwork)
-                .ToString();
-
-        private static void UploadPostgres(NpgsqlConnection connection, string data){
-          var command = connection.CreateCommand();
-            try
-            {
-                command.CommandText = "INSERT INTO votes ( vote) VALUES ( @vote)";
-                command.Parameters.AddWithValue("@vote", data);
-                Console.WriteLine(data);
-                command.ExecuteNonQuery();
-            }
-            catch (DbException ex)
-            {
-                Console.WriteLine("No se puede ");
-                Console.WriteLine("Database Exception:");
-            Console.WriteLine($"Message: {ex.Message}");
-            Console.WriteLine($"Source: {ex.Source}");
-            Console.WriteLine($"ErrorCode: {ex.ErrorCode}");
-            }
-            finally
-            {
-                command.Dispose();
-            }
-
-        }
-        
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
