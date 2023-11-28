727dd09f2d6c61d27476be63d5ba33e0c4e8cf0b
Aldo Ramos <aldoramosaro@gmail.com>
2023-11-28 18:18:25 -0500

    Update app.py

--- procesing/app.py
+++ procesing/app.py
@@ -14,15 +14,18 @@ gunicorn_error_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers.extend(gunicorn_error_logger.handlers)
 app.logger.setLevel(logging.INFO)
 
-def get_redis():
+def get_redis_broker():
     if not hasattr(g, 'redis'):
         #cambiar el puerto por 6380 para comectarce al otro redis
-        g.redis = Redis(host="redis", port=6379, db=0, socket_timeout=5)
+        g.redis = Redis(host="redis-collect", port=6379, db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
 def hello():
-  return 
+    redis = get_redis_broker()
+    valor = redis.get('getUsuarioCursos')
+    
+  return make_response(jsonify({'message':'API procesamiento de datos'})) 
 
 
 if __name__ == "__main__":
