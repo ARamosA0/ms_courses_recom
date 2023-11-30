3f969f88bc0be641c92ef95ee28474414211812d
ARamosA0 <aldoramosaro@gmail.com>
2023-11-30 17:03:16 +0000

    aaaa

--- procesing/app.py
+++ procesing/app.py
@@ -25,13 +25,10 @@ def get_redis_broker():
     return g.redis
 
 ###
+
 def get_data(input_file):
-  inicio = time.time()
   ratings = dd.read_table(input_file, sep='\t', assume_missing=True, names=['userId', 'movieId', 'rating', 'rating_timestamp'])
   ratings_pandas = ratings.compute()#ratings_pandas = ratings.compute()
-  fin = time.time()
-  print(fin-inicio)
-  print(ratings.head)
   return ratings.head()
  
 def consolidate_data(df):
@@ -39,16 +36,72 @@ def consolidate_data(df):
     consolidated_df = df.groupby(['userId', 'movieId'])['rating'].mean().unstack()
     return consolidated_df
 
+def limpia(np1, np2):
+    mask = ~np.isnan(np2)
+    np1 = np1[mask]
+    np2 = np2[mask]
 
+    np1, np2 = np2, np1
+
+    mask = ~np.isnan(np2)
+    np1 = np1[mask]
+    np2 = np2[mask]
+
+    np1, np2 = np2, np1
+
+    return pd.DataFrame({'A': np1, 'B': np2})
+
+def computeManhattanDistance(ax, bx):
+    return np.sum(np.abs(ax - bx))
+
+def computeNearestNeighbor(username, users_df):
+    user_data = np.array(users_df.loc[username])
+    distances = np.empty((users_df.shape[0],), dtype=float)
+
+    for i, (index, row) in enumerate(users_df.iterrows()):
+        if index != username:
+            ax = np.array(row)
+            bx = np.array(user_data)
+            temp = limpia(ax, bx)
+            ax = np.array(temp["A"].tolist())
+            bx = np.array(temp["B"].tolist())
+            distances[i] = computeManhattanDistance(ax, bx)
+
+    sorted_indices = np.argsort(distances)
+    sorted_distances = distances[sorted_indices]
+
+    return list(zip(sorted_distances, users_df.index[sorted_indices]))
+
+
+def recommend(username, users_df):
+    nearest_neighbors = computeNearestNeighbor(username, users_df)
+    user_data = np.array(users_df.loc[username])
+    user_items = np.isnan(user_data)
+    recommendations = {}
+    for distance, neighbor in nearest_neighbors:
+        neighbor_data = np.array(users_df.loc[neighbor])
+        neighbor_items = np.isnan(neighbor_data)
+        new_items = np.logical_and(neighbor_items, ~user_items)
+        for item_index, has_new_item in enumerate(new_items):
+            if has_new_item:
+                if item_index not in recommendations:
+                    recommendations[item_index] = 0
+                recommendations[item_index] += neighbor_data[item_index] / (distance + 1)
+    sorted_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)
+    recommended_items = [item_index for item_index, score in sorted_recommendations]
+    return recommended_items
 ###
 
 @app.route("/", methods=['POST','GET'])
 def hello():
     #redis = get_redis_broker()
     #valor = redis.get('getUsuarioCursos')
-    df=get_data("ratings.dat")
-    #recommended_items = computeNearestNeighbor(1, df)
-    #print(recommended_items)
+    inicio = time.time()
+    df=get_data('ratings.dat')
+    fin = time.time()
+    print(fin-inicio)
+    items = computeNearestNeighbor(1, df)
+    print(items)
     return make_response(jsonify({'message':'API procesamiento de datos'})) 
 
 
